34df030cb6055abae4e2c52541df69166d424713
Stephen Gordon <sgordon@redhat.com>
2016-12-28 18:07:57 -0500

    Minor cleanliness and comment updates

--- ovirt-hosts-generate.py
+++ ovirt-hosts-generate.py
@@ -2,6 +2,11 @@ import argparse
 from ovirtsdk.api import API
 
 def _parseArguments():
+    """
+    Parse command line arguments using argparse, these parameters currently
+    mimic those of the command line client for the oVirt/RHEV API and relate
+    primarily to initiating the API connection.
+    """
 
     parser = argparse.ArgumentParser(description="Automatically generate" +
                                      "Ansible hosts file for all guests that" +
@@ -18,9 +23,17 @@ def _parseArguments():
     return parser.parse_args()
 
 def main():
+    """
+    Initiate API connection and retrieve list of guests that have IP address
+    information available.
+    """
 
     args = _parseArguments()
 
+    # Initiate API connection, some of these values will be empty depending on
+    # the type of the connection being used (particularly secure versus
+    # insecure). Using insecure=True (by specifying -I on the command line) is
+    # the easiest way to get up and running but of course trades off security.
     api = API(url=args.location,
               username=args.user,
               password=args.password,
@@ -32,16 +45,21 @@ def main():
     # Retrieve all guests that are up and showing an IP
     vms = api.vms.list(query="ip != \"\" and status = \"up\"")
 
+    # Print a basic group header for the hosts.
+    # TODO: Separate groupings for each guest OS type.
     print("[rhev-guests]")
 
+    # Loop through all the returned VMs.
     for vm in vms:
 
         # Technically this check that the guest agent info is present should
-        # not be needed, as if it game back as having an IP above the agent
+        # not be needed, as if it came back as having an IP above the agent
         # must be there.
         if vm.get_guest_info():
 
             # Get the list of IP addresses for the guest.
+            # TODO: The structure of this would suggest we might want to verify
+            #       how robust it is if more than one record comes back etc.
             ips = vm.get_guest_info().get_ips().get_ip()
             for ip in ips:
                 print("%s # %s" % (ip.get_address(), vm.name))
